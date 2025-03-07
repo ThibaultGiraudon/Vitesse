36cbca4def71fc2adf2932188dec1b72bc28b6ae
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-03-07 14:10:31 +0100

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Thibault Giraudon on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
