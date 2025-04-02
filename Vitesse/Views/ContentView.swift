6ff73ecfba0c9595a5c71db3090eb43e41655066
ThibaultGiraudon <thibault.giraudon@gmail.com>
2025-04-02 15:34:17 +0200

    Add logout button

--- Vitesse/Views/ContentView.swift
+++ Vitesse/Views/ContentView.swift
@@ -11,13 +11,26 @@ struct ContentView: View {
     @ObservedObject var viewModel = AppViewModel(user: User.shared)
     @StateObject var user = User.shared
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottomTrailing) {
             Group {
                 if user.isLoggedIn {
                     CandidatesView()
                 } else {
                     LoginView(viewModel: viewModel.authViewModel)
                 } 
+            }
+            if user.isLoggedIn {
+                Image(systemName: "rectangle.portrait.and.arrow.right")
+                    .onTapGesture {
+                        User.shared.isAdmin = false
+                        User.shared.token = ""
+                        User.shared.isLoggedIn = false
+                    }
+                    .padding()
+                    .background(.white)
+                    .clipShape(Circle())
+                    .shadow(radius: 5)
+                    .padding()
             }
         }
     }
